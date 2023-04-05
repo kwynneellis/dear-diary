0b5faf150e65781ca8fd41016b87147cb02293f1
Kat WynneEllis <k.wynneellis@gmail.com>
2023-04-05 18:26:42 +0100

    adds validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :email, :password, :first_name, :last_name, :date_of_birth, presence: true
+  validates :email, :username, uniqueness: true
 end
