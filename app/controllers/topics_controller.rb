9d107a90a2a7671757708eb80470e6e3cc228b23
Kat W-E <kat@fonix.com>
2024-05-18 17:44:15 +0100

    topic CRUD methods, views and validation

---
+++ app/controllers/topics_controller.rb
@@ -0,0 +1,45 @@
+class TopicsController < ApplicationController
+  before_action :set_topic, only: %i[show edit update]
+
+  def index
+    @topics = Topic.all
+  end
+
+  def show
+  end
+
+  def new
+    @topic = Topic.new
+  end
+
+  def create
+    @topic = Topic.new(topic_params)
+    
+    if @topic.save
+      redirect_to @topic
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @topic.update(topic_params)
+      redirect_to @topic
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_topic
+    @topic = Topic.find(params[:id])
+  end
+
+  def topic_params
+    params.require(:topic).permit(:name, :description, :is_active, :is_default, :is_int, :is_string, :is_text, :is_float, :is_duration, :is_boolean, :min_scale, :max_scale, :is_list)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  
   # Defines the root path route ("/")
   # root "articles#index"
   root "pages#about"
+
+  resources :topics, only: [:index, :show, :new, :create, :edit, :update]
 end
